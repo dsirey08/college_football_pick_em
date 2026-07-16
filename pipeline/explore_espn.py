50d1778d6261ade688afd5849130b5118f585c80
Dave Sirey <david.sirey@gmail.com>
2026-07-15 23:30:25 -0700

    Explore ESPN scoreboard API structure

--- pipeline/explore_espn.py
+++ pipeline/explore_espn.py
@@ -1,15 +1,17 @@
 import requests
 import json
 
-url = "https://site.api.espn.com/apis/site/v2/sports/football/college-football/rankings"
+url = "https://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard?dates=20241005"
 
 response = requests.get(url)
 data = response.json()
 
-rankings = data["rankings"]
+events = data['events']
+first_game = events[0]
+competition = first_game["competitions"][0]
 
-for ranking in rankings:
-    print("Poll name:", ranking["name"])
-
-print("\nFirst team in first poll:")
-print(json.dumps(rankings[0]["ranks"][0], indent=2))
+print("Team 1 details:")
+print("Home/Away:", competition["competitors"][0]["homeAway"])
+print("Winner:", competition["competitors"][0]["winner"])
+print("Score:", competition["competitors"][0]["score"])
+print("Team:", competition["competitors"][0]["team"]["id"], competition["competitors"][0]["team"]["displayName"])
